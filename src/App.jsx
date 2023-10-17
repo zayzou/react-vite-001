4cd6c39f4eb34ec4a03bb89df948d707d6ade4cb
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-16 21:36:13 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import ErrorExample from "./tutorial/01-useState/starter/01-error-example"
+import Starter from "./tutorial/01-useState/starter/02-useState-basics"
 function App() {
   return (
     <div className="container">
-      <ErrorExample/>
+      <Starter/>
     </div>
   )
 }
--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+
+  // useState(1) is an array of value,function
+  // console.log(...useState(1));
+  // const value = useState("bob")[0] //the actual value
+  // const func = useState("bob")[1] //the function that will update the state.
+  // console.log(value);
+  // console.log(func);
+
+  const [count,setCount] = useState(0);
+
+  const handleClick= ()=>{
+    setCount(count+1)
+  }
+  return <div>
+    <h2>you clicked {count}</h2>
+    <button className="btn" onClick={handleClick}>Increment</button>
+  </div>;
 };
 
 export default UseStateBasics;
