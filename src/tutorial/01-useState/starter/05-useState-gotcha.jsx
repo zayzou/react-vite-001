b9d8e73aa6a00c0bc58632e8e7dc761b0cf02876
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-19 16:43:39 -0400

    complete gotcha session

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+
+  const increment = () => {
+    setValue((currentState)=>{
+      let newState = currentState+1
+      console.log(newState);
+      return newState
+    });
+  };
+  return (
+    <>
+      <h2>useState "gotcha"</h2>
+      <h5>{value}</h5>
+      <button className="btn" onClick={increment}>
+        increment
+      </button>
+    </>
+  );
 };
 
 export default UseStateGotcha;
