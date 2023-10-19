0a6367acce3ab60f60bd43d93b4bbea631e3bb13
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-19 18:50:34 -0400

    complete 05

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -3,17 +3,25 @@ const UseStateGotcha = () => {
   const [value, setValue] = useState(0);
 
   const increment = () => {
-    setValue((currentState)=>{
-      let newState = currentState+1
+    setValue((currentState) => {
+      let newState = currentState + 1;
       console.log(newState);
-      return newState
+      return newState;
     });
+  };
+  const incrementWithTimeout = () => {
+    setTimeout(() => {
+      console.log("clicked the button");
+      setValue((current) => {
+        return current + 1;
+      });
+    }, 3000);
   };
   return (
     <>
-      <h2>useState "gotcha"</h2>
+      <h2>useState gotcha</h2>
       <h5>{value}</h5>
-      <button className="btn" onClick={increment}>
+      <button className="btn" onClick={incrementWithTimeout}>
         increment
       </button>
     </>
