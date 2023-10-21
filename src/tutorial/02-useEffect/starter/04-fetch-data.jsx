35a98295814c15f069669cbfd2b880e96aae9367
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-21 13:06:32 -0400

    complete fetch github users

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,23 +1,37 @@
-import { useState, useEffect } from "react";
-const url = "https://api.github.com/users";
+import { useState,useEffect } from "react";
 
-const FetchData = () => {
-  const [users, setUsers] = useState([]);
+const url = 'https://api.github.com/users';
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((result) => {
-        setUsers(result);
-      });
-  }, []);
+
+const FetchData = () =>{
+
+  const [users,setUsers] = useState([])
+
+  useEffect(()=>{
+    fetch(url)
+    .then(response =>response.json())
+    .then(data=>setUsers(data))
+  },[])
 
   return (
-    <>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </>
-  );
-};
-export default FetchData;
+    <main>
+      <h4>Fetch data example</h4>
+      <h3>Github Users</h3>
+      <ul className="users">
+        {
+          users.map((user)=>{
+            return <li key={user.id} className="person">
+            <img src={user.avatar_url} alt="avatar url"  className="img"/>
+            <div>
+            <h5>{user.login}</h5>
+            <a href={user.url}>Profile</a>
+            </div>
+          </li>
+          })
+        }
+      </ul>
+    </main>
+  )
+}
+
+export default FetchData
