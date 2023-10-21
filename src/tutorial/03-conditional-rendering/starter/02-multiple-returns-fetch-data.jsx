224da524d4b25a1c9e7c2fb45904096491cef405
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-21 17:17:56 -0400

    complete multiple return fetch data 01

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,41 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from "react";
+
+// const url = "https://api.github.com/users/QuincyLarson";
+const url = "https://jsonplaceholder.typicode.com/users/1";
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
+      });
+  }, []);
+
+  if (user === null) {
+    return <h2>Loading ...</h2>;
+  }
+
+  return (
+    <div className="container">
+      <h4>Fetch Data</h4>
+      <section className="">
+        <img
+          src="https://www.course-api.com/images/people/person-4.jpeg"
+          alt={user.name}
+          className="img"
+          style={{ width: "140px", height: "140px", margin: "auto" }}
+        />
+        <h3>{user.name}</h3>
+        <h4>Works at {user.company.name}</h4>
+        <h5>
+          Find more on <a href={user.website}>{user.website}</a>
+        </h5>
+        <p>{user.company.catchPhrase}</p>
+      </section>
+    </div>
+  );
 };
+
 export default MultipleReturnsFetchData;
