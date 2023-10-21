23bd9922fa02968447b1f6a17a0d08bc5503f556
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-21 18:54:04 -0400

    add header info to fetch

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const url = "https://api.githb.com/users/QuincyLarson";
+const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
   const [user, setUser] = useState(null);
@@ -8,7 +8,18 @@ const MultipleReturnsFetchData = () => {
   const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    fetch(url)
+    const myHeaders = new Headers();
+    myHeaders.append(
+      "Cookie",
+      "_octo=GH1.1.2094383103.1678821344; logged_in=no"
+    );
+
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+    fetch(url, requestOptions)
       .then((response) => response.json())
       .then((data) => {
         setUser(data);
@@ -28,22 +39,23 @@ const MultipleReturnsFetchData = () => {
     return <h2>An error occurred 🥺.</h2>;
   }
 
+  const { avatar_url, name, company, blog, bio } = user;
   return (
     <div className="container">
       <h4>Fetch Data</h4>
       <section className="">
         <img
-          src={user.avatar_url}
-          alt={user.name}
+          src={avatar_url}
+          alt={name}
           className="img"
           style={{ width: "140px", height: "140px", margin: "auto" }}
         />
-        <h3>{user.name}</h3>
-        <h4>Works at {user.company}</h4>
+        <h3>{name}</h3>
+        <h4>Works at {company}</h4>
         <h5>
-          Find more on <a href={user.blog}>{user.blog}</a>
+          Find more on <a href={blog}>{blog}</a>
         </h5>
-        <p>{user.bio}</p>
+        <p>{bio}</p>
       </section>
     </div>
   );
