c8f0ed0cf36ab0b35f06ce90ebd7f1c316e22ccd
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-22 15:59:25 -0400

    Complete toggle challenge

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [user] = useState({ name: "John" });
+
+  return (
+    <div>
+      <h2>toggle challenge</h2>
+      <button className="btn" onClick={() => setLoggedIn(!isLoggedIn)}>
+        {isLoggedIn ? "Logout" : "Login"}
+      </button>
+      {isLoggedIn && <User name={user.name} />}
+    </div>
+  );
 };
+
+const User = ({ name }) => (
+  <div className="alert alert-success">Hi {name} 👋🏻</div>
+);
 
 export default ToggleChallenge;
