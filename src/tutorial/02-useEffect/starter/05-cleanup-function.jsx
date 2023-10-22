b18ab8fabc6cc5b14b7f1e6af84bb2a5f44a988d
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-22 18:12:48 -0400

    Complete cleanup function

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,32 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  console.log("Render ");
+  const [isLogged, setIsLogged] = useState(false);
+  return (
+    <div>
+      <button className="btn" onClick={() => setIsLogged(!isLogged)}>
+        Toggle
+      </button>
+      {isLogged && <SecondComponent />}
+    </div>
+  );
+};
+
+const SecondComponent = () => {
+  useEffect(() => {
+    console.log("hmm, this is interesting");
+    const intId = setInterval(() => {
+      console.log("Call from setInterval");
+    }, 1000);
+    return () => {
+      console.log("Cleanup");
+      clearInterval(intId);
+    };
+  }, []);
+
+  return <h4>Second Component !</h4>;
 };
 
 export default CleanupFunction;
