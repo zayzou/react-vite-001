722cf21c0c5e9887d41142fbbdc1be723aee125f
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-22 16:20:34 -0400

    Complete user challenge

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const handleLogin = () => {
+    if (!isLoggedIn) {
+      console.log("Fetching user data ...");
+      fetch("https://jsonplaceholder.typicode.com/users/1")
+        .then((response) => response.json())
+        .then((data) => setUser(data));
+    } else {
+      console.log("Destroying user data ...");
+      setUser(null);
+    }
+    setIsLoggedIn(!isLoggedIn);
+  };
+  return (
+    <div>
+      <h2>user challenge</h2>
+      <button className="btn" onClick={handleLogin}>
+        {isLoggedIn ? "logout" : "login"}
+      </button>
+      {isLoggedIn && <UserCard {...user} />}
+    </div>
+  );
 };
 
+const UserCard = ({ name, email, phone }) => {
+  return (
+    <div>
+      <h4 className="">{name}</h4>
+      <h5>
+        reach me at {email} | {phone}
+      </h5>
+    </div>
+  );
+};
 export default UserChallenge;
