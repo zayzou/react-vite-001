1066bd4684b100a6f688f0466d0e3a3e67dc0d52
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-12-17 17:53:11 -0500

    Suspense API

--- src/tutorial/11-performance/starter/04-react-18/index.jsx
+++ src/tutorial/11-performance/starter/04-react-18/index.jsx
@@ -1,8 +1,11 @@
-import { useState, useTransition } from "react";
+import { useState, useTransition, Suspense, lazy } from "react";
+const SlowComponent = lazy(() => import("./SlowComponent"));
+// import SlowComponent from "./SlowComponent";
 const LatestReact = () => {
   const [text, setText] = useState("");
   const [items, setItems] = useState([]);
   const [isPending, startTransition] = useTransition();
+  const [toggle, setToggle] = useState(false);
   const handleChange = (e) => {
     setText(e.target.value);
 
@@ -19,31 +22,36 @@ const LatestReact = () => {
     });
   };
   return (
-    <section>
-      <form className="form">
-        <input
-          type="text"
-          className="form-input"
-          value={text}
-          onChange={handleChange}
-        />
-      </form>
-      <h4>Items Below</h4>
-
-      {isPending ? (
-        <h3>Loading ....</h3>
-      ) : (
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr 1fr",
-            marginTop: "2rem",
-          }}
-        >
-          {items}
-        </div>
-      )}
-    </section>
+    <Suspense fallback={<h3>Loading ....</h3>}>
+      <section>
+        <form className="form">
+          <input
+            type="text"
+            className="form-input"
+            value={text}
+            onChange={handleChange}
+          />
+        </form>
+        <h4>Items Below</h4>
+        {isPending ? (
+          <h3>Loading ....</h3>
+        ) : (
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "1fr 1fr 1fr",
+              marginTop: "2rem",
+            }}
+          >
+            {items}
+          </div>
+        )}
+        <button className="btn" onClick={() => setToggle(!toggle)}>
+          Toggle
+        </button>
+        {toggle && <SlowComponent />}
+      </section>
+    </Suspense>
   );
 };
 export default LatestReact;
